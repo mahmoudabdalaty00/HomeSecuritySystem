537f809a77d28be0a9bee29bf9657c21078f0f97
mahmoudabdalaty00 <117662685+mahmoudabdalaty00@users.noreply.github.com>
2025-01-22 18:48:31 +0200

    Add project files.

---
+++ HomeSecuritySystem.Domain/Device.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeSecuritySystem.Domain
+{
+    public  class Device
+    {
+        [Key]
+        public int Id { get; set; } 
+       
+        public string DeviceType { get; set; }
+      
+        public string Model { get; set; }
+       
+        public bool Status { get; set; }
+       
+        public int UserId { get; set; }
+ 
+    }
+}
