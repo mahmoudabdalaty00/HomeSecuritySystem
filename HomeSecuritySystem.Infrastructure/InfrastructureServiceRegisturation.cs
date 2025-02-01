20dfebec5d110fb52d39c1351437c35d70b15110
mahmoudabdalaty00 <117662685+mahmoudabdalaty00@users.noreply.github.com>
2025-02-01 23:06:39 +0200

    Add part  of Logging in action

--- HomeSecuritySystem.Infrastructure/InfrastructureServiceRegisturation.cs
+++ HomeSecuritySystem.Infrastructure/InfrastructureServiceRegisturation.cs
@@ -1,6 +1,8 @@
 ﻿using HomeSecuritySystem.Application.Contracts.Email;
+using HomeSecuritySystem.Application.Contracts.Logging;
 using HomeSecuritySystem.Application.Models.Emailmodels;
 using HomeSecuritySystem.Infrastructure.EmailService;
+using HomeSecuritySystem.Infrastructure.Logging;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -16,9 +18,19 @@ namespace HomeSecuritySystem.Infrastructure
                 configuration.GetSection("EmailSettings"));
             services.AddTransient<IEmailSender, EmailSender>();
 
+            services.AddScoped(typeof(IAppLogger<>), typeof(LoggerAdapter<>));
+            /* 
+             we should Select where we put our login action (success , fail , details )_
+                1- we can put it in the controller
+                2- we can put it in the service
+                3- we can put it in the repository
+                4- we can put it in the infrastructure
+                5- we can put it in the application
 
+            in the door or using Ai  as you like 
 
-
+             
+             */
 
             // Add your infrastructure services here
             return services;
---
+++ HomeSecuritySystem.Infrastructure/Logging/LoggerAdapter.cs
@@ -0,0 +1,36 @@
+﻿using HomeSecuritySystem.Application.Contracts.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace HomeSecuritySystem.Infrastructure.Logging
+{
+    public class LoggerAdapter<T> : IAppLogger<T>
+    {
+        private readonly  ILogger<T> _logger;
+        public LoggerAdapter(ILoggerFactory logger)
+        {
+            _logger = logger.CreateLogger<T>();
+        }
+        public void LogInformation(string message, params object[] args)
+        {
+            _logger.LogInformation(message, args);
+        }
+        public void LogWarning(string message, params object[] args)
+        {
+            _logger.LogWarning(message, args);
+        }
+        public void LogError(string message, params object[] args)
+        {
+            _logger.LogError(message, args);
+        }
+        public void LogError(Exception ex, string message, params object[] args)
+        {
+            _logger.LogError(ex, message, args);
+        }
+
+    }
+}
